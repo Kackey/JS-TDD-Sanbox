3e684a9c8f2c6af8f18a5af674af56a45e16ca69
Hidekazu Kakinuma <hidekazu.kakinuma@mail.rakuten.com>
2013-12-08 17:25:46 +0900

    - Add library sets of "Jasmine" and "Backbone.js" - Add some intotroductional sandbox code.

---
+++ jasmine-sandbox/spec/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hidekazu.kakinuma on 13/12/08.
+ */
+
+describe("Hello world", function() {
+    it("says hello", function(){
+        expect(helloWorld()).toEqual("Hello World!");
+    });
+});
---
+++ jasmine-sandbox/src/HelloWorld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hidekazu.kakinuma on 13/12/08.
+ */
+
+function helloWorld(){
+    return "Hello World!";
+}
