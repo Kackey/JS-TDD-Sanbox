c3eddf87e266af348ea9e3af71a2d3ca123c56b5
Kackey <hide.kaki0910@gmail.com>
2014-04-01 17:33:13 +0900

    Add simple sample code for cucumber.js

---
+++ Cucumber.js-sandbox/tests/features/support/world.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hide on 2014/04/01.
+ */
+
+var World = function (callback) {
+    //set up cod goes here
+
+    //last line to tell cucumber.js the World is ready.
+    callback(this);
+}
+
+exports.World = World;
